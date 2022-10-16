2d233d4e6fd6db9bd962cc6b3a4ac0708552fa41
YaroslavTaranovskiy <103592817+YaroslavTaranovskiy@users.noreply.github.com>
2022-10-16 16:15:52 +0300

    doing-2

--- src/authFirebase/app.js
+++ src/authFirebase/app.js
@@ -1,4 +1,5 @@
-import { authWithEmailAndPassword } from './auth'
+import { authWithEmailAndPassword, logInUserWithEmail } from './auth'
+
 
 const authModalOpenBtn = document.getElementById('auth-open-modal');
 
@@ -7,7 +8,7 @@ authModalOpenBtn.addEventListener('click', openModal)
 
 function getAuthForm() {
     return `
-         <button class="modal-close__btn" data-close-modal__window>
+         <button class="modal-close__btn js-modal-close" data-close-modal__window>
                 <svg width="20" height="20" class="swiper__icon">
                     <use href="../images/sprite.svg#icon-close"></use>
                 </svg>
@@ -52,4 +53,60 @@ function openModal() {
         .getElementById('auth-modal')
         .addEventListener('submit', authFormHandler)
     
+}
+
+const logInBtn = document.getElementById('log-open-modal')
+
+logInBtn.addEventListener('click', openLogInModal)
+
+function getLogInForm() {
+     return `
+         <button class="modal-close__btn js-modal-close" data-close-modal__window>
+                <svg width="20" height="20" class="swiper__icon">
+                    <use href="../images/sprite.svg#icon-close"></use>
+                </svg>
+            </button>
+            <form class="auth-modal" id="auth-form">
+                <div class="input-area-email">
+                    <label for="email" class="label-email">E-mail</label>
+                    <input type="text" id="email">
+                </div>
+                <div class="input-area-password">
+                    <label for="password">Password</label>
+                    <input type="text" id="password">
+                </div>
+                <button class="button button-auth-modal-btn" type="submit">
+                    Log in
+                </button>
+            </form>
+    `
+}
+
+function openLogInModal(event) {
+    console.log(event);
+    createLogInModal('Log in your account', getLogInForm())
+    document
+        .getElementById('login-modal')
+        .addEventListener('submit', logInFormHandler)
+    
+}
+
+function createLogInModal(title, content) {
+    console.log(content);
+    const modal = document.getElementById('login-modal');
+    modal.innerHTML = `
+    <h2 class="auth-title">${title}</h2>
+    ${content}
+            
+    `
+}
+
+
+function logInFormHandler(event) {
+    event.preventDefault()
+
+    const email = event.target.querySelector('#email').value
+    const password = event.target.querySelector('#password').value
+
+    logInUserWithEmail(email, password)
 }
--- src/authFirebase/auth.js
+++ src/authFirebase/auth.js
@@ -24,3 +24,28 @@ export function authWithEmailAndPassword(email, password) {
     .catch(e => alert(e));
 }
 
+export function logInUserWithEmail(email, password) {
+  const apiKey = `AIzaSyCHg4J_qzob1qjMAXidJJyR0ERxgFjrLiU`;
+  return fetch(
+    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        password,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+    .then(r => {
+      if (r.ok) {
+        return alert('Enjoy watching movies. Grab more popcorn and snacks.');
+      } else {
+        throw new Error(`Email or Password is wrong`);
+      }
+    })
+    .catch(e => alert(e));
+}
